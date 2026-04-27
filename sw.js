26b48fd314cb11241618ff0275be9f4d216b49c0
Saroj Kumar <saroj1011535@gmail.com>
2026-04-27 22:01:09 +0530

    Implement caching in service worker install event
    Add caching for todo app files during service worker installation

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
 self.addEventListener("install", function(e) {
-    console.log("Service Worker: Installed");
+    console.log("Service Worker Installed");
+
+    e.waitUntil(
+        caches.open("todo-cache").then(function(cache) {
+            return cache.addAll([
+                "/todo-app/",
+                "/todo-app/index.html",
+                "/todo-app/style.css",
+                "/todo-app/script.js"
+            ]);
+        })
+    );
 });
