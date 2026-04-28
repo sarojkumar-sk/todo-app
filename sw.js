d8d2892f2617275fd39fae590c1de2e16be4e212
Saroj Kumar <saroj1011535@gmail.com>
2026-04-28 07:46:15 +0530

    Add icon.png to service worker cache during install

--- sw.js
+++ sw.js
@@ -1,14 +1,21 @@
 self.addEventListener("install", function(e) {
-    console.log("Service Worker Installed");
+  e.waitUntil(
+    caches.open("todo-cache").then(function(cache) {
+      return cache.addAll([
+        "/todo-app/",
+        "/todo-app/index.html",
+        "/todo-app/style.css",
+        "/todo-app/script.js",
+        "/todo-app/icon.png"
+      ]);
+    })
+  );
+});
 
-    e.waitUntil(
-        caches.open("todo-cache").then(function(cache) {
-            return cache.addAll([
-                "/todo-app/",
-                "/todo-app/index.html",
-                "/todo-app/style.css",
-                "/todo-app/script.js"
-            ]);
-        })
-    );
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
